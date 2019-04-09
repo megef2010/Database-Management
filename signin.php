b4359220c175181941b9aeac62a76592246a1e99
drhead <1313496+drhead@users.noreply.github.com>
2019-04-09 13:15:52 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,68 +1,16 @@
 <?php
+	session_name('MetubeSession');
 	session_start();
-	session_name($MeTubeSignedIn);
 
-	$myDB = new mysqli("mysql1.'http://metube-1223456.duckdns.org/'", "metube", "SuperSecurePassword");
-
-	function getDB(){
-		//file path to json file
-		$dbpath = 'private/database.json';
-
-		//load it
-		$db = file_get_contents($dbpath);
-
-		//if blank, use empty array, else json decode into array
-		if($db === "" || $db === false){
-			$db = Array();
-		}else{
-			$db = json_decode($db,true);
-		}
-
-		return $db;
-	}
-
-	function writeDB($db){
-		//file path to json file
-		$dbpath = 'private/database.json';
-
-		//write it
-		file_put_contents($dbpath, json_encode($db));
-	}
-	
-	function saveUser($user){
-		$db = getDB();
-
-		//write obj into users array (db array)
-		$db[$user['username']] = $user;
-
-		//save to disk
-		writeDB($db);
-
-		return true;
-	}
-
-	function authUser($username,$password){
-		$db = getDB();
-		if(!isset($db[$username])){
-			return "No User";
-		}
-
-		$password = $password.$db[$username]['salt'];
-		$hash = hash("sha256", $password);
-
-		if($hash !== $db[$username]['hash']){
-			return "No Match";
-		}
-
-		return true;
-	}
-
+	$myDB = new mysqli('localhost', 'metube', 'SuperSecurePassword', 'metube');
 
 	$username = $_POST['email'];
+	$username = $myDB->real_escape_string($username);
 	$password = $_POST['password'];
+	$password = md5($password);
 
 	
-	if(authUser($username, $password) !== true){
+	if($myDB->query("SELECT COUNT(*) FROM users WHERE name = '$username' AND password = '$password'") !== 1){
 
 		session_unset();
 		session_destroy();
@@ -70,15 +18,14 @@
 		exit();
 	}
 	
-	$usernameSQL = $myDB->real_escape_string($username);
-	$sql = "SELECT ID FROM SPONSORS WHERE EMAIL='$usernameSQL'";
+	$sql = "SELECT id FROM users WHERE name='$username'";
 	$results = $myDB->query($sql);
 	$arr = $results->fetch_assoc();
 
-	$id = $arr['ID'];
+	$id = $arr['id'];
 
 	$_SESSION['username'] = $username;
-	$_SESSION['sponsorID'] = $id;
+	$_SESSION['userID'] = $id;
 	echo '	<a href="MeTube.html">Go To Account</a>	';
 	exit();
 ?>
