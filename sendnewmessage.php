0155e705617aaf7e35ebba3429a048dcb5b81fff
megef2010 <42848821+megef2010@users.noreply.github.com>
2019-04-14 14:21:11 -0400

    Update sendnewmessage.php

--- sendnewmessage.php
+++ sendnewmessage.php
@@ -1,14 +1,20 @@
 <?php
 	session_name('MetubeSession');
 	session_start();
-	$myDB = new mysqli('localhost', 'metube', 'SuperSecurePassword', 'metube');
+	$myDB = mysqli_connect('localhost', 'metube', 'SuperSecurePassword', 'metube');
+	if (mysqli_connect_errno()) {
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  	}
 
 	$sender = $_POST['id'];
 	$receiver = $_POST['to'];
 	$message = $_POST['$messagebody'];
 
-	$myDB->query("INSERT INTO messages (sendid, recvid, text) VALUES ('$sender', '$receiver', '$message')");
-  header('Location: MeTubeMessage.html?errorMessage="Email Sent"', true, 200);
+	$receiverid = mysqli_query($myDB, "SELECT id FROM user WHERE name=$receiver");
+
+	$myDB->query("INSERT INTO messages (sendid, recvid, text) VALUES ('$sender', '$receiverid', '$message')");
+  	header('Location: MeTubeMessage.html?errorMessage="Email Sent"', true, 200);
+	mysqli_close($myDB);
 	exit();
 ?>
 
