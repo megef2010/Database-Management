990d1ab6ecec1629a78e8c6305b50593e091dce0
megef2010 <42848821+megef2010@users.noreply.github.com>
2019-05-01 11:05:10 -0400

    Create MeTubeChangePassword.php

---
+++ MeTubeChangePassword.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MeTube/Password Change</title>
+	<link rel="stylesheet" type="text/css" href="MeTube.css">
+</head>
+
+<body>
+	<?php include 'MeTube_GlobalHeader.php';?>
+	<br><br>
+  <p><form class="newpass" action="operation.php" align="center" method="post">
+    New Password <input type="password" name="password" required><br><br>
+    Confirm New Password <input type="password" name="conpassword" required><br><br>
+    <input type="hidden" name="action" value="newpass"/>
+    <input type="submit" value="Submit"><br><br>
+  </form></p>
+
+</body>
+</html>
