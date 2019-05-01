05a102a9cbea975f06302884aa0e1e6a85e470cc
megef2010 <42848821+megef2010@users.noreply.github.com>
2019-05-01 11:23:52 -0400

    Update MeTubeChangePassword.php

--- MeTubeChangePassword.php
+++ MeTubeChangePassword.php
@@ -9,13 +9,15 @@
 
 <body>
 	<?php include 'MeTube_GlobalHeader.php';?>
-	<br><br>
-  <p><form class="newpass" action="operation.php" align="center" method="post">
-    New Password <input type="password" name="password" required><br><br>
-    Confirm New Password <input type="password" name="conpassword" required><br><br>
-    <input type="hidden" name="action" value="newpass"/>
-    <input type="submit" value="Submit"><br><br>
-  </form></p>
+	<?php if(@$_GET['err'] == 1) {
+	echo 'Passwords do not match.';
+	}?>
+	<p><form class="newpass" action="operation.php" align="center" method="post">
+		New Password <input type="password" name="password" required><br><br>
+		Confirm New Password <input type="password" name="conpassword" required><br><br>
+		<input type="hidden" name="action" value="newpass"/>
+		<input type="submit" value="Submit"><br><br>
+	</form></p>
 
 </body>
 </html>
