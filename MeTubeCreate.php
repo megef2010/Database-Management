2c519667ffe8994fd2b3963bf30d1144a340ad40
megef2010 <42848821+megef2010@users.noreply.github.com>
2019-05-01 10:54:01 -0400

    Update MeTubeCreate.php

--- MeTubeCreate.php
+++ MeTubeCreate.php
@@ -8,9 +8,15 @@
 
 <body>
 	<?php include 'MeTube_GlobalHeader.php'; ?>
+	<?php if(@$_GET['err'] == 1) {
+	echo 'Username is taken.';
+	}?>
+	<?php if(@$_GET['err'] == 2) {
+	echo 'Passwords do not match.';
+	}?>
 	<form class="signin" action="operation.php" method="post" align="center">
 		<!-- Links to PHP file for signing in. -->
-		<p>Username <input type="text" name="username" required><br><br> Password <input type="password" name="password" required><br><br> Description <input type="text" name="description"><br><br>
+		<p>Username <input type="text" name="username" required><br><br> Password <input type="password" name="password" required><br><br>Confirm Password <input type="password" name="conpassword" required><br><br> Description <input type="text" name="description"><br><br>
 			<input type="hidden" name="action" value="create"/>
 			<input type="submit" value="Submit">
 		</p>
