eb68b4cb796125ca679d1b7a1c054652aaa9d277
megef2010 <42848821+megef2010@users.noreply.github.com>
2019-05-01 12:13:20 -0400

    Update MeTubeMessage.php

--- MeTubeMessage.php
+++ MeTubeMessage.php
@@ -17,7 +17,16 @@
 
 	<div id="newMessage">
 		<form class="newMessage" action="operation.php" method="post">
-			<input type="text" name="to" placeholder="To..."><br><br>
+			<select name="to" required>
+				<option value="">To...</option>
+				<?php 
+				if ($contacts = mysql_query("SELECT users.name FROM users LEFT JOIN usercontacts ON userid=". $_SESSION['userID'] ."WHERE users.id=usercontacts.contactid")) {
+					while ($con_row = mysql_fetch_row($contacts)) {
+				?>
+				<option value="<?php echo $con_row[0]; ?>">
+				</option>
+				<?php } } ?>
+			</select>
 			<input type="text" name="subject" placeholder="Subject"><br><br>
 			<textarea name="messagebody" cols="40" rows="5" placeholder="Your message here"></textarea><br><br>
 			<input type="hidden" name="action" value="message"/>
