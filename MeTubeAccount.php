ce7cefe5d05435909ddf4d1c5c205f76c1ff0160
megef2010 <42848821+megef2010@users.noreply.github.com>
2019-05-02 22:00:46 -0400

    Update MeTubeAccount.php

--- MeTubeAccount.php
+++ MeTubeAccount.php
@@ -33,7 +33,7 @@
 			if ( isset( $_SESSION[ 'userID' ] ) ) {?>
 			<form class="contact" action="operation.php" method="post">
 				<?php
-		      			if ( mysql_query("SELECT COUNT(*) FROM usercontacts WHERE userid=". $_SESSION[ 'userID' ] . " AND contactid='$id'") != 0 ) {?>
+		      			if ( mysql_result(mysql_query("SELECT COUNT(*) FROM usercontacts WHERE userid=". $_SESSION[ 'userID' ] . " AND contactid='$id'"), 0) != 0 ) {?>
 						<input type="hidden" name="action" value="addcontacts"/>
 						<input type="hidden" name="contact" value="<?php echo '$id'; ?>"/>
 						<input type="submit" name="submit" value="Add To Contacts"/>
